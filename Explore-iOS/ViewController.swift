2f5f6b09ba53daf53b9c7b8b090539cc547b07cf
Kiley Caravella <kiley@intrepid.io>
2017-03-21 11:10:14 -0400

    Initial Commit

---
+++ Explore-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Explore-iOS
+//
+//  Created by Kiley Caravella on 3/21/17.
+//  Copyright © 2017 kileycaravella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
