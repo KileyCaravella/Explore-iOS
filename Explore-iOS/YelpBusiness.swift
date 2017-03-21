c434bf9f32ea478e5465158f93e58717e4f991a8
Kiley Caravella <kiley@intrepid.io>
2017-03-21 18:09:11 -0400

    first commit

---
+++ Explore-iOS/YelpBusiness.swift
@@ -0,0 +1,9 @@
+//
+//  YelpBusiness.swift
+//  Explore-iOS
+//
+//  Created by Kiley Caravella on 3/21/17.
+//  Copyright © 2017 kileycaravella. All rights reserved.
+//
+
+import Foundation
---
+++ Explore-iOS/YelpClient.swift
@@ -0,0 +1,10 @@
+//
+//  APIClient.swift
+//  Explore-iOS
+//
+//  Created by Kiley Caravella on 3/21/17.
+//  Copyright © 2017 kileycaravella. All rights reserved.
+//
+
+import Foundation
+
