6a8bab8b785ccf35b4f7ba11a2cca07e117f2f52
Kiley Caravella <kiley@intrepid.io>
2017-03-21 19:57:42 -0400

    setup YelpBusiness and YelpBusinessLocation models

--- Explore-iOS/YelpBusiness.swift
+++ Explore-iOS/YelpBusiness.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class YelpBusiness {
+    var id: String
+    var name: String
+    var reviewCount: Int
+    var rating: Double
+    var distance: Double
+    var categories: [String:Any]
+    var url: String
+    var phone: String
+    var displayPhone: String
+    var isClosed: Bool
+    var location: YelpBusinessLocation
+    var imageURL: String
+    var coordinates: [String:Any]
+    
+    init(js: [String:Any]) {
+        id = js["id"] as? String ?? ""
+        name = js["name"] as? String ?? ""
+        reviewCount = js["review_count"] as? Int ?? 0
+        rating = js["rating"] as? Double ?? 0.0
+        distance = js["distance"] as? Double ?? 0.0
+        categories = js["categories"] as? [String:Any] ?? [:]
+        url = js["url"] as? String ?? ""
+        phone = js["phone"] as? String ?? ""
+        displayPhone = js["display_phone"] as? String ?? ""
+        isClosed = js["is_closed"] as? Bool ?? true
+        
+        let jsLocation = js["location"] as? [String:Any] ?? [:]
+        location = YelpBusinessLocation(js: jsLocation)
+
+        imageURL = js["image_url"] as? String ?? ""
+        coordinates = js["coordinates"] as? [String:Any] ?? [:]
+    }
+}
--- Explore-iOS/YelpBusinessLocation.swift
+++ Explore-iOS/YelpBusinessLocation.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class YelpBusinessLocation {
+    var address1: String
+    var address2: String
+    var address3: String
+    var city: String
+    var state: String
+    var zipCode: String
+    var country: String
+    var displayAddress: [String]
+    
+    init(js: [String:Any]) {
+        address1 = js["address1"] as? String ?? ""
+        address2 = js["address2"] as? String ?? ""
+        address3 = js["address3"] as? String ?? ""
+        city = js["city"] as? String ?? ""
+        state = js["state"] as? String ?? ""
+        zipCode = js["zip_code"] as? String ?? ""
+        country = js["country"] as? String ?? ""
+        displayAddress = js["display_address"] as? [String] ?? []
+    }
+}
